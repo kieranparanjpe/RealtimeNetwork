cbec3508b8a1de9b475f756a1b980b006bb7bd50
Kieran Paranjpe <927985@learn.vsb.bc.ca>
2021-01-04 15:00:24 -0800

    idek

---
+++ Project/src/NetworkHandler.java
@@ -0,0 +1,16 @@
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.sun.tools.javac.Main;
+import processing.core.*;
+
+import java.lang.reflect.Method;
+import java.util.List;
+import com.fasterxml.jackson.databind.*;
+
+
+public class NetworkHandler
+{
+    private MainSuper sketch;
+
+
+}
